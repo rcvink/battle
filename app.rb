29412cc78ac572fbdc26fdf3275099cf95dad924
rcvink <rrvink@gmail.com>
2018-01-18 11:47:06 +0000

    Refactor to model layer

--- app.rb
+++ app.rb
@@ -1,4 +1,5 @@
 require 'sinatra/base'
+require './lib/player'
 
 class Battle < Sinatra::Base
   enable :sessions
@@ -8,15 +9,15 @@ class Battle < Sinatra::Base
   end
 
   post '/names' do
-   session[:player_1_name] = params[:player_1_name]
-   session[:player_2_name] = params[:player_2_name]
+   $player1 = Player.new(params[:player_1_name])
+   $player2 = Player.new(params[:player_2_name])
    redirect to'/play'
   end
 
   get '/play' do
     @player_health = 100
-    @player_1_name = session[:player_1_name]
-    @player_2_name = session[:player_2_name]
+    @player_1_name = $player1.name
+    @player_2_name = $player2.name
     erb :play
   end
 
